54599c2de95de3daf7e93678c638f87c6b389895
vistajess <vistajesus8@gmail.com>
2016-01-26 02:28:46 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,363 @@
+	var table = [
+				"", "", "1.00794", 1, 1,
+				"", "", "4.002602", 18, 1,
+				"", "", "6.941", 1, 2,
+				"", "", "9.012182", 2, 2,
+				"", "", "10.811", 13, 2,
+				"", "", "12.0107", 14, 2,
+				"", "", "14.0067", 15, 2,
+				"", "", "15.9994", 16, 2,
+				"", "", "18.9984032", 17, 2,
+				"", "", "20.1797", 18, 2,
+				"", "", "22.98976...", 1, 3,
+				"", "Magnesium", "24.305", 2, 3,
+				"", "Aluminium", "26.9815386", 13, 3,
+				"", "Silicon", "28.0855", 14, 3,
+				"", "Phosphorus", "30.973762", 15, 3,
+				"", "Sulfur", "32.065", 16, 3,
+				"", "Chlorine", "35.453", 17, 3,
+				"", "Argon", "39.948", 18, 3,
+				"", "Potassium", "39.948", 1, 4,
+				"", "Calcium", "40.078", 2, 4,
+				"", "Scandium", "44.955912", 3, 4,
+				"", "Titanium", "47.867", 4, 4,
+				"", "Vanadium", "50.9415", 5, 4,
+				"", "Chromium", "51.9961", 6, 4,
+				"", "Manganese", "54.938045", 7, 4,
+				"", "Iron", "55.845", 8, 4,
+				"", "Cobalt", "58.933195", 9, 4,
+				"", "Nickel", "58.6934", 10, 4,
+				"", "Copper", "63.546", 11, 4,
+				"", "Zinc", "65.38", 12, 4,
+				"", "Gallium", "69.723", 13, 4,
+				"", "Germanium", "72.63", 14, 4,
+				"", "Arsenic", "74.9216", 15, 4,
+				"", "Selenium", "78.96", 16, 4,
+				"", "Bromine", "79.904", 17, 4,
+				"", "Krypton", "83.798", 18, 4,
+				"", "Rubidium", "85.4678", 1, 5,
+				"", "Strontium", "87.62", 2, 5,
+				"", "Yttrium", "88.90585", 3, 5,
+				"", "Zirconium", "91.224", 4, 5,
+				"", "Niobium", "92.90628", 5, 5,
+				"", "Molybdenum", "95.96", 6, 5,
+				"", "Technetium", "(98)", 7, 5,
+				"", "Ruthenium", "101.07", 8, 5,
+				"", "Rhodium", "102.9055", 9, 5,
+				"", "Palladium", "106.42", 10, 5,
+				"", "Silver", "107.8682", 11, 5,
+				"", "Cadmium", "112.411", 12, 5,
+				"", "Indium", "114.818", 13, 5,
+				"", "Tin", "118.71", 14, 5,
+				"", "Antimony", "121.76", 15, 5,
+				"", "Tellurium", "127.6", 16, 5,
+				"", "Iodine", "126.90447", 17, 5,
+				"", "Xenon", "131.293", 18, 5,
+				"", "Caesium", "132.9054", 1, 6,
+				"", "Barium", "132.9054", 2, 6,
+				"", "Lanthanum", "138.90547", 4, 9,
+				"", "Cerium", "140.116", 5, 9,
+				"", "Praseodymium", "140.90765", 6, 9,
+				"", "Neodymium", "144.242", 7, 9,
+				"", "Promethium", "(145)", 8, 9,
+				"", "Samarium", "150.36", 9, 9,
+				"", "Europium", "151.964", 10, 9,
+				"", "Gadolinium", "157.25", 11, 9,
+				"", "Terbium", "158.92535", 12, 9,
+				"", "Dysprosium", "162.5", 13, 9,
+				"", "Holmium", "164.93032", 14, 9,
+				"", "Erbium", "167.259", 15, 9,
+				"", "Thulium", "168.93421", 16, 9,
+				"", "Ytterbium", "173.054", 17, 9,
+				"", "Lutetium", "174.9668", 18, 9,
+				"", "Hafnium", "178.49", 4, 6,
+				"", "Tantalum", "180.94788", 5, 6,
+				"", "Tungsten", "183.84", 6, 6,
+				"", "Rhenium", "186.207", 7, 6,
+				"", "Osmium", "190.23", 8, 6,
+				"", "Iridium", "192.217", 9, 6,
+				"", "Platinum", "195.084", 10, 6,
+				"", "Gold", "196.966569", 11, 6,
+				"", "Mercury", "200.59", 12, 6,
+				"", "Thallium", "204.3833", 13, 6,
+				"", "Lead", "207.2", 14, 6,
+				"", "Bismuth", "208.9804", 15, 6,
+				"", "Polonium", "(209)", 16, 6,
+				"", "Astatine", "(210)", 17, 6,
+				"", "Radon", "(222)", 18, 6,
+				"", "Francium", "(223)", 1, 7,
+				"", "Radium", "(226)", 2, 7,
+				"", "Actinium", "(227)", 4, 10,
+				"", "Thorium", "232.03806", 5, 10,
+				"", "Protactinium", "231.0588", 6, 10,
+				"", "Uranium", "238.02891", 7, 10,
+				"", "Neptunium", "(237)", 8, 10,
+				"", "Plutonium", "(244)", 9, 10,
+				"", "Americium", "(243)", 10, 10,
+				"", "Curium", "(247)", 11, 10,
+				"", "Berkelium", "(247)", 12, 10,
+				"", "Californium", "(251)", 13, 10,
+				"", "Einstenium", "(252)", 14, 10,
+				"", "Fermium", "(257)", 15, 10,
+				"", "Mendelevium", "(258)", 16, 10,
+				"", "Nobelium", "(259)", 17, 10,
+				"", "Lawrencium", "(262)", 18, 10,
+				"", "Rutherfordium", "(267)", 4, 7,
+				"", "Dubnium", "(268)", 5, 7,
+				"", "Seaborgium", "(271)", 6, 7,
+				"", "Bohrium", "(272)", 7, 7,
+				"", "Hassium", "(270)", 8, 7,
+				"", "Meitnerium", "(276)", 9, 7,
+				"", "Darmstadium", "(281)", 10, 7,
+				"", "Roentgenium", "(280)", 11, 7,
+				"", "Copernicium", "(285)", 12, 7,
+				"", "Unutrium", "(284)", 13, 7,
+				"", "Flerovium", "(289)", 14, 7,
+				"", "Ununpentium", "(288)", 15, 7,
+				"", "Livermorium", "(293)", 16, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununseptium", "(294)", 17, 7,
+				"", "Ununoctium", "(294)", 18, 7
+			];
+
+			var camera, scene, renderer;
+			var controls;
+
+			var objects = [];
+			var targets = { table: [], sphere: [], helix: [], grid: [] };
+
+			init();
+			animate();
+			
+			function init() {
+
+				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+				camera.position.z = 3000;
+
+				scene = new THREE.Scene();
+
+				var images = ['images/leaf.png','images/leaf2.png','images/leaf4.png','images/leaf5.png','images/leaf7.png'];
+				// table
+
+				for ( var i = 0; i < table.length; i+=5) {
+
+					var element = document.createElement( 'div' );
+					element.className = 'element';
+					element.style.backgroundImage = 'url(' +  images[Math.ceil(Math.random() * (4 - 0) + 0)]  + ')';
+
+					var object = new THREE.CSS3DObject( element );
+					object.position.x = Math.random() * 3000 - 2000;
+					object.position.y = Math.random() * 2000 - 800;
+					object.position.z = Math.random() * 1000 - 2000;
+
+					scene.add( object );
+
+					objects.push( object );
+					//
+
+					var object = new THREE.Object3D();
+					// object.position.x = ( table[ i + 3 ] * 140 ) - 1330;
+					// object.position.y = - ( table[ i + 4 ] * 180 ) + 990;
+
+					object.position.x = ( table[ i + 3] * 90) - 990;
+					object.position.y = - ( table[ i + 3] * 90 ) + 500;
+
+
+					targets.table.push( object );
+
+				}
+
+				// sphere
+
+				var vector = new THREE.Vector3();
+				console.log(objects.length)
+				Object.assign(objects,objects);
+				for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+					var phi = Math.acos( -1 + ( 2 * i ) / l );
+					var theta = Math.sqrt( l * Math.PI ) * phi;
+
+					var object = new THREE.Object3D();
+
+					object.position.x = 700 * Math.cos( theta ) * Math.sin( phi );
+					object.position.y = 700 * Math.sin( theta ) * Math.sin( phi );
+					object.position.z = 700 * Math.cos( phi );
+
+					vector.copy( object.position ).multiplyScalar( 2 );
+
+					object.lookAt( vector );
+
+					targets.sphere.push( object );
+
+				}
+
+				// helix
+
+				var vector = new THREE.Vector3();
+
+				for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+					var phi = i * 0.175 + Math.PI;
+
+					var object = new THREE.Object3D();
+
+					object.position.x = 900 * Math.sin( phi );
+					object.position.y = - ( i * 8 ) + 450;
+					object.position.z = 900 * Math.cos( phi );
+
+					vector.x = object.position.x * 2;
+					vector.y = object.position.y;
+					vector.z = object.position.z * 2;
+
+					object.lookAt( vector );
+
+					targets.helix.push( object );
+
+				}
+
+				// grid
+
+				for ( var i = 0; i < objects.length; i ++ ) {
+
+					var object = new THREE.Object3D();
+
+					object.position.x = ( ( i % 5 ) * 400 ) - 800;
+					object.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
+					object.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
+
+					targets.grid.push( object );
+
+				}
+
+				//
+
+				renderer = new THREE.CSS3DRenderer();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.domElement.style.position = 'absolute';
+				document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+				//
+
+				controls = new THREE.TrackballControls( camera, renderer.domElement );
+				controls.rotateSpeed = 0.5;
+				controls.minDistance = 500;
+				controls.maxDistance = 6000;
+				controls.addEventListener( 'change', render );
+
+				var button = document.getElementById( 'table' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.table, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'sphere' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.sphere, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'helix' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.helix, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'grid' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.grid, 2000 );
+
+				}, false );
+
+				transform( targets.table, 2000 );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function transform( targets, duration ) {
+
+				TWEEN.removeAll();
+
+				for ( var i = 0; i < objects.length; i ++ ) {
+
+					var object = objects[ i ];
+					var target = targets[ i ];
+
+					new TWEEN.Tween( object.position )
+						.to( { x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration )
+						.easing( TWEEN.Easing.Exponential.InOut )
+						.start();
+
+					new TWEEN.Tween( object.rotation )
+						.to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+						.easing( TWEEN.Easing.Exponential.InOut )
+						.start();
+
+				}
+
+				new TWEEN.Tween( this )
+					.to( {}, duration * 2 )
+					.onUpdate( render )
+					.start();
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+				render();
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				TWEEN.update();
+
+				controls.update();
+				// object.rotation.x += 0.1;
+				// object.rotation.y += 0.1;
+
+			}
+
+			function render() {
+
+				renderer.render( scene, camera );
+
+			}
+
+var container = $("#container");
+function bounce() {
+	setInterval(function() {
+    container.animate({'top':'15%'},1000)
+    container.animate({'top':'0%'},1000)
+	}, 3000);
+}
+
+function styleFunc() {
+	document.getElementById('container').style.top = '5%';
+}
+
+document.querySelector('#sphere').addEventListener('click',function() {
+	bounce();
+});
